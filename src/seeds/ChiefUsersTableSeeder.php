8a4425cfee3ad4aea1a63333300a231c5f1988a7
Ben Cavens <BenCavens@users.noreply.github.com>
2015-05-19 21:25:53 +0200

    removed Thomas user

--- src/seeds/ChiefUsersTableSeeder.php
+++ src/seeds/ChiefUsersTableSeeder.php
@@ -15,17 +15,6 @@ class ChiefUsersTableSeeder extends Seeder {
                 'updated_at' => '2013-09-11 15:50:47',
                 'avatar'    => '',
                 'slug' => 'bencavens',
-            ),
-             array(
-                'email' => 'tielemansthomas@gmail.com',
-                'password' => '$2y$10$Qm5JAlG7NdSw4893fqe03.idp/sKQfz69b/qS7nrEX0kun8F.4gPG',
-                'first_name' => 'Thomas',
-                'last_name' => 'Tielemans',
-                'activated' => 1,
-                'created_at' => '2013-09-12 11:32:56',
-                'updated_at' => '2013-09-12 11:45:24',
-                'avatar'    => '',
-                'slug' => 'thomastielemans'
             )
         );
 
